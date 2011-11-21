b6fe659f28d292761b9dc16481b89b9558ba0a30
NKCN42 <nathanielkyleng@gmail.com>
2011-11-21 14:08:28 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NCNfinalProject::Application.config.secret_token = 'fc76f24842efa56f42b7b265d88b309d94045cc7c2c90753b96579f4b00831b263c8f95b07bf91d8aa179f8e03533e52a8a7f73dd1248ecd6ed934db06d41b4c'
