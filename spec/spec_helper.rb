5a2842f3f92682757e0b917f5a9825abb4af1451
NKCN42 <nathanielkyleng@gmail.com>
2011-11-21 21:25:30 -0600

    Saving spork stuff

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,9 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'spork'
 
+Spork.prefork do
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
@@ -30,4 +32,5 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+end
 end
