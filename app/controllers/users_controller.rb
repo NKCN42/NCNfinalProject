b4967c96417f78398657c595069bd46cc7a56245
NKCN42 <nathanielkyleng@gmail.com>
2011-12-15 13:31:04 -0600

    Added 'Need login' for control panel

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:show]
   def new
     
   end
@@ -6,5 +7,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @microposts = @user.microposts
     @title = "Control Panel"
+  end
+  private
+  def authenticate 
+    deny_access unless signed_in?
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -27,7 +27,6 @@ module SessionsHelper
   end
   
   def deny_access
-    store_location
     redirect_to signin_path, :notice => "Please sign in to access this page"
   end
   
